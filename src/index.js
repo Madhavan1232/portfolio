fd431b520a6e048447510d86697c998f84957b76
Madhavan1232 <madhavanvairavan3@gmail.com>
2026-01-12 10:57:13 +0530

    final deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import App from './App.js';
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import {HashRouter } from "react-router-dom";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
